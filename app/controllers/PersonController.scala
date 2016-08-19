be4fe367d68ebc6bc3aefc56ebde17093f298ee2
Willem Jan Glerum <willem-jan.glerum@lunatech.com>
2016-08-19 18:27:06 +0200

    Save pictures

--- app/controllers/PersonController.scala
+++ app/controllers/PersonController.scala
@@ -1,34 +1,25 @@
 package controllers
 
 import java.io.File
-
-import play.api._
-import play.api.mvc._
-import play.api.i18n._
-import play.api.data.Form
-import play.api.data.Forms._
-import play.api.data.validation.Constraints._
-import play.api.libs.json.Json
-import models._
-import dal._
-
-import scala.concurrent.{ExecutionContext, Future}
 import javax.inject._
 
+import dal.PersonRepository
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.i18n._
 import play.api.libs.Files
+import play.api.libs.json.Json
+import play.api.mvc._
+
+import scala.concurrent.ExecutionContext
 
 class PersonController @Inject()(repo: PersonRepository, val messagesApi: MessagesApi)
                                 (implicit ec: ExecutionContext) extends Controller with I18nSupport {
-
-  //not needed.. embedded in play/scala
-  def isValidEmail(email: String): Boolean =
-  """(\w+)@([\w\.]+)""".r.unapplySeq(email).isDefined
 
   /**
     * The mapping for the person form.
     */
   val personForm: Form[CreatePersonForm] = Form {
-    //    println("debug ---4---")
     mapping(
       "name" -> nonEmptyText,
       "email" -> email,
@@ -40,17 +31,12 @@ class PersonController @Inject()(repo: PersonRepository, val messagesApi: Messag
     * The index action.
     */
   def index = Action.async {
-    //    println("debug ---3---")
     println()
     repo.list().map(people =>
       Ok(views.html.index(personForm, people)))
-    //    println("")
   }
 
-
   def getDetailedPerson(id: Long) = Action.async {
-    //    println("debug ---person ID---: " + id.toString)
-    //    val person = Person(id,"ali", 10, "ali.jpeg")
     repo.get(id).map {
       case Some(person) => Ok(views.html.detailedPerson(person))
       case None => NotFound("Person Not Found")
@@ -75,13 +61,11 @@ class PersonController @Inject()(repo: PersonRepository, val messagesApi: Messag
       // There were no errors in the from, so create the person.
       // at this point we have successful person object from html strings name and age
       person => {
-        //        println("debug ---6---" + request.body.toString)
         val temp = uploadFile(request)
-        //println("debug ---110--- " + temp)
         repo.create(person.name, person.email, temp, person.description).map { person =>
           println(person.toString)
           // If successful, we simply redirect to the index page.
-          Redirect(routes.PersonController.index)
+          Redirect(routes.PersonController.index())
         }
 
       }
@@ -89,27 +73,17 @@ class PersonController @Inject()(repo: PersonRepository, val messagesApi: Messag
   }
 
   def uploadFile(request: Request[MultipartFormData[Files.TemporaryFile]]): String = {
-    val name = request.body.file("photo").map { picture =>
-      import java.io.File
-      val filenameToReturn = picture.filename
+    request.body.file("photo").map { picture =>
+      if(!picture.filename.isEmpty) {
 
+        val filenameToUpload = play.Play.application.configuration.getString("pictures_path") + picture.filename
 
-      val filenameToUpload = play.Play.application.configuration.getString("pictures_path") + picture.filename
-
-      //            println("debug ---101--- " + filenameToUpload)
-      //            //println("debug ---102--- " + routes.Assets.versioned("images/"))
-      //            println("debug ---103--- " + picture.ref.file.getAbsolutePath)
-      //            println("debug ---104--- " + play.Play.application.configuration.getString("pictures_path"))
-      val contentType = picture.contentType
-      //      val file = new File(s"public/images/$filename")
-      val file = new File(filenameToUpload)
-      picture.ref.moveTo(file)
-      filenameToReturn
-    }
-    val n = name.getOrElse("ali.jpeg")
-    if (n.isEmpty) "ali.jpeg"
-    else n
-    //name.getOrElse ("")
+        val file = new File(filenameToUpload)
+        picture.ref.moveTo(file)
+        picture.filename
+      }
+      else "ali.jpeg"
+    }.getOrElse("ali.jpeg")
   }
 
   /**
@@ -131,10 +105,8 @@ class PersonController @Inject()(repo: PersonRepository, val messagesApi: Messag
 
 
   def giveMePicture(name: String) = Action {
-    //        println("debug ---1---"+name)
-    //    val myFile:File = new File("/Users/alihassan/tmp/pictures/file000844922903.jpg")
     val myFile: File =
-    if (!(name == "ali.jpeg"))
+    if (!(name == "ali.jpeg" || name == ""))
       new File(play.Play.application.configuration.getString("pictures_path") + name)
     else
       new File("public/images/ali.jpeg")
