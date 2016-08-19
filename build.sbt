b4a085b3e296eb669a66e1a59428d7d4f850c039
ahassa01 <alihtb@gmail.com>
2016-08-19 14:30:02 +0200

    update

--- build.sbt
+++ build.sbt
@@ -11,7 +11,8 @@ libraryDependencies ++= Seq(
   "com.typesafe.play" %% "play-slick-evolutions" % "2.0.0",
   "com.h2database" % "h2" % "1.4.190",
   "org.xerial" % "sqlite-jdbc" % "3.7.2",
-  "postgresql" % "postgresql" % "9.1-901.jdbc4",
+//  "postgresql" % "postgresql" % "9.1-901.jdbc4",
+  "org.postgresql" % "postgresql" % "9.4-1201-jdbc41",
     specs2 % Test
 )
 
--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,15 +1,19 @@
 # --- !Ups
 
 create table "people" (
-  "id" INTEGER PRIMARY KEY   AUTOINCREMENT,
+--  "id" INTEGER PRIMARY KEY   AUTO_INCREMENT,
+  "id" SERIAL PRIMARY KEY  ,
   "name" varchar not null ,
   "email" varchar not null,
   "photo" varchar not null,
   "description" varchar
 );
 
+
+
+
 # --- !Downs
 
-drop table "people" if exists;
+drop table "people";
 
 
